bd3d08437f94e286355d2cb0fd560781cbb77f5b
Fatima Fneish <fatimafneish.95@hotmail.com>
2016-12-27 15:39:43 +0200

    clickable phone and email done by haidar

--- app/src/main/java/com/mc/info/lumc/DoctorInfo.java
+++ app/src/main/java/com/mc/info/lumc/DoctorInfo.java
@@ -1,22 +1,25 @@
 package com.mc.info.lumc;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class DoctorInfo extends AppCompatActivity {
 
     private int index;
     private MyDatabaseHandler dbHandler = new MyDatabaseHandler(this,null,null,1);
-    private TextView txt;
+    private TextView txt,call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_info);
-        setTitle("Doctor's Page");
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             index = extras.getInt("take");
@@ -27,12 +30,42 @@ public class DoctorInfo extends AppCompatActivity {
             txt.setText(d.getLastName());
             txt = (TextView) findViewById(R.id.activity_doctor_info_specialty);
             txt.setText(d.getSpecialty());
-            txt = (TextView) findViewById(R.id.activity_doctor_info_phone);
-            txt.setText(d.getPhone());
+            call = (TextView) findViewById(R.id.activity_doctor_info_phone);
+            call.setText(d.getPhone());
+
+            call.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+call.getText().toString()));
+                    startActivity(callIntent);
+
+                }
+            });
+
+
             txt = (TextView) findViewById(R.id.activity_doctor_info_address);
             txt.setText(d.getAddress().toString());
             txt = (TextView) findViewById(R.id.activity_doctor_info_email);
             txt.setText(d.getEmail());
+
+            txt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        emailIntent.setType("message/rfc822");
+                        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{txt.getText().toString()});
+                        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+                        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+                        startActivity(Intent.createChooser(emailIntent,"Sending Email"));
+                    } catch (ActivityNotFoundException ex){
+                        Toast toast=   Toast.makeText(DoctorInfo.this,"Cannot Connect ",Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+
+            });
+
         }
     }
 }
--- app/src/main/java/com/mc/info/lumc/PatientInfo.java
+++ app/src/main/java/com/mc/info/lumc/PatientInfo.java
@@ -1,14 +1,19 @@
 package com.mc.info.lumc;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PatientInfo extends AppCompatActivity {
 
     private int index;
     private MyDatabaseHandler dbHandler = new MyDatabaseHandler(this,null,null,1);
-    private TextView txt;
+    private TextView txt,call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +28,42 @@ public class PatientInfo extends AppCompatActivity {
             txt = (TextView) findViewById(R.id.activity_patient_info_LastName);
             txt.setText(p.getLastName());
             txt = (TextView) findViewById(R.id.activity_patient_info_Phone);
-            txt.setText(p.getPhone());
-            txt = (TextView) findViewById(R.id.activity_patient_info_Address);
+            call = (TextView) findViewById(R.id.activity_doctor_info_phone);
+            call.setText(p.getPhone());
+
+            call.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+call.getText().toString()));
+                    startActivity(callIntent);
+
+                }
+            });
+
+
+            txt = (TextView) findViewById(R.id.activity_doctor_info_address);
             txt.setText(p.getAddress().toString());
-            txt = (TextView) findViewById(R.id.activity_patient_info_email);
+            txt = (TextView) findViewById(R.id.activity_doctor_info_email);
             txt.setText(p.getEmail());
+
+            txt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        emailIntent.setType("message/rfc822");
+                        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{txt.getText().toString()});
+                        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+                        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+                        startActivity(Intent.createChooser(emailIntent,"Sending Email"));
+                    } catch (ActivityNotFoundException ex){
+                        Toast toast=   Toast.makeText(PatientInfo.this,"Cannot Connect ",Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+
+            });
+
         }
     }
 }
-
