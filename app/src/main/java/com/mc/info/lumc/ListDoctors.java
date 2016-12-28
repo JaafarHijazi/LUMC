dba87fb4dba1c04ac4424e3aaea4af982fa8eb18
mahdi saab <mahdisaab5@gmail.com>
2016-12-28 19:49:38 +0200

    In this version I've changed : 1-instead of using simpleListAdapter I've used a customAdapter with some changes on the codes. 2-added 2 classes one for the adapter of the patients and one for the doctors. 3-some changes on the layout of the pateintList and DoctorList

--- app/src/main/java/com/mc/info/lumc/ListDoctors.java
+++ app/src/main/java/com/mc/info/lumc/ListDoctors.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,26 +22,30 @@ public class ListDoctors extends AppCompatActivity {
     private ArrayList<Doctor> doctors;
     private ArrayList<HashMap<String, String>> data = new ArrayList<>();
     private SimpleAdapter adapter;
-
+    private DoctorListAdapter adapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_doctors);
         SearchView sv = (SearchView) findViewById(R.id.activity_list_doctors_search);
-
         doctors = dbHandler.getAllDoctors();
+        adapter1 = new DoctorListAdapter(this,doctors);
 
-        for (int i = 0; i < doctors.size(); i++) {
-            data.add(doctors.get(i).toHashMap());
+
+       /*
+
+        for (int i = 0; i < patients.size(); i++) {
+            data.add(patients.get(i).toHashMap());
         }
 
 
-        String[] hash = {MyDatabaseHandler.COLUMN_FIRST_NAME, MyDatabaseHandler.COLUMN_LAST_NAME , MyDatabaseHandler.COLUMN_SPECIALTY};
-        int[] toViewIds = {R.id.list_doctor_item_txtFname, R.id.list_doctor_item_txtLname , R.id.list_doctor_item_specialty};
-        adapter = new SimpleAdapter(this, data, R.layout.list_doctor_item, hash, toViewIds);
+         String[] hash = {MyDatabaseHandler.COLUMN_FIRST_NAME, MyDatabaseHandler.COLUMN_LAST_NAME};
+        int[] toViewIDs = {R.id.list_item_patient_firstName, R.id.list_item_patient_lastName};
+        adapter = new SimpleAdapter(this, data, R.layout.list_patients_item, hash, toViewIDs);
+        */
         lv = (ListView) findViewById(R.id.activity_list_doctors_doctorList);
-        lv.setAdapter(adapter);
+        lv.setAdapter(adapter1);
 
 
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -61,7 +66,9 @@ public class ListDoctors extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String str = adapter.getItem(i).toString();
+ /*               String str = adapter1.getItem(i).toString();
+                Toast.makeText(ListDoctors.this,str,Toast.LENGTH_SHORT).show();
+
                 Doctor d;
                 int index=str.lastIndexOf(MyDatabaseHandler.COLUMN_ID+"=");
                 int last=str.indexOf(",",index);
@@ -69,12 +76,13 @@ public class ListDoctors extends AppCompatActivity {
                     last=str.indexOf("}",index);
                 }
                 String idString = str.substring(index+MyDatabaseHandler.COLUMN_ID.length()+1,last);
-                d = dbHandler.getDoctorById(Integer.parseInt(idString));
-                if(idString.equals(String.valueOf( d.getId()))){
-                    Intent j = new Intent(ListDoctors.this, DoctorInfo.class);
-                    j.putExtra("take",d.getId());
-                    startActivity(j);
-                }
+                d = dbHandler.getDoctorById(Integer.parseInt(idString));*/
+                Doctor d=adapter1.getItem(i);
+
+                Intent j = new Intent(ListDoctors.this, DoctorInfo.class);
+                j.putExtra("take",d.getId());
+                startActivity(j);
+
             }
         });
     }
@@ -94,16 +102,9 @@ public class ListDoctors extends AppCompatActivity {
             doctors = dbHandler.sortDoctorsBy(MyDatabaseHandler.COLUMN_LAST_NAME);
         else if(item.getItemId() == R.id.menu_list_doctors_BySpecialty)
             doctors = dbHandler.sortDoctorsBy(MyDatabaseHandler.COLUMN_SPECIALTY);
-        data = new ArrayList<>();
-        for (Doctor d1 : doctors) {
-            data.add(d1.toHashMap());
-        }
-
-        String[] hash = {MyDatabaseHandler.COLUMN_FIRST_NAME, MyDatabaseHandler.COLUMN_LAST_NAME , MyDatabaseHandler.COLUMN_SPECIALTY};
-        int[] toViewIds = {R.id.list_doctor_item_txtFname, R.id.list_doctor_item_txtLname , R.id.list_doctor_item_specialty};
-        adapter = new SimpleAdapter(this, data, R.layout.list_doctor_item, hash, toViewIds);
+        adapter1 = new DoctorListAdapter(this,doctors);
         lv = (ListView) findViewById(R.id.activity_list_doctors_doctorList);
-        lv.setAdapter(adapter);
+        lv.setAdapter(adapter1);
         return true;
     }
 
